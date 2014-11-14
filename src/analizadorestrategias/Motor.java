0373349da072d0421068123b77214d4ed3b48294
David Espinel <David Espinel@localhost>
2014-11-14 11:35:41 -0500

    - Archivos iniciales

---
+++ src/analizadorestrategias/Motor.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package analizadorestrategias;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Motor {
+    
+     public static Analizador ui;
+    public static void main(String[] args) {
+            ui = new Analizador();
+
+	}
+}
